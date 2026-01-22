edc5cb929a35cdbd2c62efd343cdb2e591642110
Fr1gat3 <dutrest20@gmail.com>
2026-01-22 19:31:03 +0300

    added PostgreSQL

--- auth_system/settings.py
+++ auth_system/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'auth_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'auth_system_db',
+        'USER': 'auth_system_username',
+        'PASSWORD': 'auth_system_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
