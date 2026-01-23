22eb9de10e0e3617557be12307e18d729b4e8ba7
Fr1gat3 <dutrest20@gmail.com>
2026-01-23 16:47:15 +0300

    created User model

--- users/models.py
+++ users/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class User(models.Model):
+
+    email = models.EmailField(unique=True)
+    password_hash = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
