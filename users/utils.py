04209ab0379d450c8bc5f5b52edcc5d3977b774c
Fr1gat3 <dutrest20@gmail.com>
2026-01-26 19:44:01 +0300

    create & decode access token

--- users/utils.py
+++ users/utils.py
@@ -1,4 +1,30 @@
 import bcrypt
+import jwt
+
+from datetime import datetime, timedelta
+from django.conf import settings
+
+JWT_SECRET = settings.SECRET_KEY
+JWT_ALGORITHM = "HS256"
+JWT_EXP_MINUTES = 60
+
+
+def create_access_token(user_id):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.utcnow() + timedelta(minutes=JWT_EXP_MINUTES),
+        "iat": datetime.utcnow(),
+    }
+    return jwt.encode(payload, JWT_SECRET, algorithm=JWT_ALGORITHM)
+
+
+def decode_access_token(token):
+    try:
+        return jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALGORITHM])
+    except jwt.ExpiredSignatureError:
+        return None
+    except jwt.InvalidTokenError:
+        return None
 
 
 def hash_password(password):
